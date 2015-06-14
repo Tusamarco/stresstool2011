fd7308bed6957cc22f3c33a8fbd68d09a0b859e6
Tusamarco <tusamarco@gmail.com>
2015-06-14 12:56:06 -0400

    Test

--- src/com/mysql/stresstool/StressTool.java
+++ src/com/mysql/stresstool/StressTool.java
@@ -194,6 +194,8 @@ private static void showHelp() {
          */
         java.util.Collections.sort(lParameters);
         
+        System.out.println("Test");
+        
         if(args.length >= 1 && args[0].indexOf("defaults-file") > 0){
 
         	try {
